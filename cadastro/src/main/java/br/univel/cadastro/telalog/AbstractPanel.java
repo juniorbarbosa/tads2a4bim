4c58cb0ddb2136bfb56528eeaf26bf3d9a389277
Junior <Junior@Junior-PC>
2015-10-22 19:35:30 -0200

    Criada a Tela Principal e o Table.

---
+++ cadastro/src/main/java/br/univel/cadastro/telalog/AbstractPanel.java
@@ -0,0 +1,50 @@
+package br.univel.cadastro.telalog;
+
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.GridBagLayout;
+import javax.swing.JButton;
+import java.awt.GridBagConstraints;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Insets;
+
+public abstract class AbstractPanel extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public AbstractPanel() {
+		setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.WHITE);
+		add(panel, BorderLayout.NORTH);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[]{0, 0, 0};
+		gbl_panel.rowHeights = new int[]{0, 0};
+		gbl_panel.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		panel.setLayout(gbl_panel);
+		
+		JLabel lblOl = new JLabel("Ol\u00E1");
+		GridBagConstraints gbc_lblOl = new GridBagConstraints();
+		gbc_lblOl.insets = new Insets(0, 0, 0, 5);
+		gbc_lblOl.gridx = 0;
+		gbc_lblOl.gridy = 0;
+		panel.add(lblOl, gbc_lblOl);
+		
+		JButton btnFechar = new JButton("Fechar");
+		GridBagConstraints gbc_btnFechar = new GridBagConstraints();
+		gbc_btnFechar.anchor = GridBagConstraints.EAST;
+		gbc_btnFechar.gridx = 1;
+		gbc_btnFechar.gridy = 0;
+		panel.add(btnFechar, gbc_btnFechar);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(Color.WHITE);
+		add(panel_1, BorderLayout.SOUTH);
+
+	}
+
+}
---
+++ cadastro/src/main/java/br/univel/cadastro/telalog/TelaPrincipal.java
@@ -0,0 +1,66 @@
+package br.univel.cadastro.telalog;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JTabbedPane;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class TelaPrincipal extends JFrame {
+
+	private JPanel contentPane;
+	private JTabbedPane tabbedPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaPrincipal frame = new TelaPrincipal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TelaPrincipal() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnCadastro = new JMenu("Cadastros");
+		menuBar.add(mnCadastro);
+		
+		JMenuItem mntmCliente = new JMenuItem("Cliente");
+		mntmCliente.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				tabbedPane.addTab("Cadastro de Cliente", new JPanel());
+			}
+		});
+		mnCadastro.add(mntmCliente);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		contentPane.add(tabbedPane, BorderLayout.CENTER);
+	}
+
+}
